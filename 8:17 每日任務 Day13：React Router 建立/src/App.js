ceedffaa78509e48b902152f1d2e546f458a2682
JimmyWu <gmwu185@gmail.com>
2022-08-17 13:05:41 +0800

    增加 React Router - NavLink, Routes, Route, 不存路由 設定

--- 8:17 每日任務 Day13：React Router 建立/src/App.js
+++ 8:17 每日任務 Day13：React Router 建立/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { HashRouter, NavLink } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
 
 const Todo = () => {
   return <p>這是 Todo 頁面</p>;
@@ -9,6 +9,9 @@ const Login = () => {
 };
 const Register = () => {
   return <p>這是註冊頁面</p>;
+};
+const About = () => {
+  return <p>這是關於我們頁面</p>;
 };
 
 function App() {
@@ -28,8 +31,26 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/about">
+            <p>關於我們</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
+        <Routes>
+          <Route path="/" element={<p>這是首頁</p>} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="todo" element={<Todo />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>不存在的路由！</p>
+              </main>
+            }
+          />
+        </Routes>
         {/* 練習區 */}
       </HashRouter>
     </div>
