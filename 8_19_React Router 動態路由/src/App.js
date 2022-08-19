6ba7addf0bc776759be7b2bfffdb95ff2901c8fb
JimmyWu <gmwu185@gmail.com>
2022-08-19 13:07:35 +0800

    1. Post 的巢狀路由 (父層 /post 與 子動態路由 /:postId)    2. 義 Post 和 PostId 兩個元件，Post 詳細資料頁面點入 /:postId 路由會顯示 Post ID 是 xxxx。

--- 8_19_React Router 動態路由/src/App.js
+++ 8_19_React Router 動態路由/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {
-  HashRouter,
+  BrowserRouter,
   NavLink,
   Routes,
   Route,
   useNavigate,
+  Outlet,
+  useParams,
 } from 'react-router-dom';
 
 const Todo = () => {
@@ -38,10 +40,23 @@ const Logout = () => {
   );
 };
 
+const Post = () => {
+  return (
+    <div>
+      <h3>Post 頁面</h3>
+      <Outlet />
+    </div>
+  );
+};
+const PostId = () => {
+  let params = useParams();
+  return <p>Post: {params.postId}</p>;
+};
+
 function App() {
   return (
     <div className="container">
-      <HashRouter>
+      <BrowserRouter>
         <div className="nav-link">
           <NavLink to="/">
             <p>回到首頁</p>
@@ -58,6 +73,12 @@ function App() {
           <NavLink to="/about">
             <p>關於我們</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post 頁面</p>
+          </NavLink>
+          <NavLink to="/post/HelloWorld">
+            <p>Post 詳細頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
@@ -66,6 +87,9 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="todo" element={<Todo />} />
           <Route path="about" element={<About />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
           <Route
             path="*"
             element={
@@ -76,7 +100,7 @@ function App() {
           />
         </Routes>
         {/* 練習區 */}
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
